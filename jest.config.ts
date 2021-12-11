944a387801ac79ccbdbaca41948af9b8b9dadaf1
Mateus Mendes <mateusmendesm16@outlook.com>
2021-12-11 10:40:04 -0300

    chore: add jest to staged

--- jest.config.ts
+++
@@ -1,9 +0,0 @@
-export default {
-  roots: ['<rootDir/src'],
-  collectCoverageFrom: ['<rootDir/src/**/*.ts>'],
-  clearMocks: true,
-  testEnvironment: 'node',
-  transform: {
-    '.+\\.ts$': 'ts-jest'
-  }
-}
